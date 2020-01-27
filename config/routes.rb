69fe101eaeb1fd1ff04659c41cceb0a94e72069e
Jean <jean-marco-10@hotmail.com>
2020-01-26 18:34:46 -0600

    Add authentication using devise and devise jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, path: "api/v1/users", controllers: { sessions: "api/v1/sessions", registrations: "api/v1/registrations" }
   namespace :api do
     namespace :v1 do
       resources :pokemons, path: "pokemon", param: :slug, only: %i[index show]
---
+++ db/migrate/20200126224925_create_jwt_blacklists.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateJwtBlacklists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :jwt_blacklist do |t|
+      t.string :jti, null: false
+      t.datetime :exp, null: false
+    end
+    add_index :jwt_blacklist, :jti
+  end
+end
